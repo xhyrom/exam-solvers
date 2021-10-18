7be9687d7f7f970474ff91b96170a3db751ed77a
xhyrom <generalkubo@gmail.com>
2021-10-18 14:48:17 +0200

    yee

--- modules/ExamBypass/src/index.js
+++ modules/ExamBypass/src/index.js
@@ -3,7 +3,9 @@ const Error = require("./structures/Error");
 
 const allAnswers = (new Answers()).get();
 
+let i = 1;
 for(let answer of allAnswers) {
     answer.resolve();
-    answer.canResolve ? new Error('[RESOLVE]', `Answer ${answer.id} has been resolved!`) : new Error('[RESOLVE]', `Answer ${answer.id} I can't resolve!`);
+    answer.canResolve ? new Error('[RESOLVE]', `Answer ${i} (${answer.id}) has been resolved!`) : new Error('[RESOLVE]', `I can't resolve answer ${i} (${answer.id})!`);
+    i++;
 }
